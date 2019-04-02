8afaebe9f593b8fb27df7ea9c820b9cd4a35bf32
andromarces <andromarces@gmail.com>
2019-04-02 23:29:11 +0800

    Changed sgapiGetValue to getvalue, and sgapiSetValue to setvalue.

--- Lua/SurveyGizmo/(AUS) Populate Cell and Primary Number.lua
+++ Lua/SurveyGizmo/(AUS) Populate Cell and Primary Number.lua
@@ -1,13 +1,13 @@
-q20 = sgapiGetValue(20) --[[ Primary contact number field ]]
-q21 = sgapiGetValue(21) --[[ Mobile field ]]
-q121 = sgapiGetValue(121) --[[ Primary contact number type dropdown ]]
+q20 = getvalue(20) --[[ Primary contact number field ]]
+q21 = getvalue(21) --[[ Mobile field ]]
+q121 = getvalue(121) --[[ Primary contact number type dropdown ]]
 primarynumberqid = 123 --[[ Primary contact number hidden field to be populated ]]
 mobilenumberqid = 122 --[[ cell hidden field to be populated ]]
 
 if (q121 == "Mobile") then
-  sgapiSetValue(primarynumberqid, q20)
-  sgapiSetValue(mobilenumberqid, q20)
+  setvalue(primarynumberqid, q20)
+  setvalue(mobilenumberqid, q20)
 else
-  sgapiSetValue(primarynumberqid, q20)
-  sgapiSetValue(mobilenumberqid, q21)
+  setvalue(primarynumberqid, q20)
+  setvalue(mobilenumberqid, q21)
 end
