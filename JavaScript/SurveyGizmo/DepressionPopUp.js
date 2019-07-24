68be23298c5613f8a4e857ace1e9e8dc61585418
Judilla-Jemifer <35318054+Judilla-Jemifer@users.noreply.github.com>
2019-07-24 05:40:57 -0500

    Update DepressionPopUp.js

--- JavaScript/SurveyGizmo/DepressionPopUp.js
+++ JavaScript/SurveyGizmo/DepressionPopUp.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
-  $(":radio").click(function(){
-    if($("input[id='sgE-4977258-5-229-10223']").is(":checked")) {
-	alert("If you or someone you know needs immediate support, please contact: \nLifeline on 13 11 14, \nSuicide Call Back Service on 1300 659 467, \nor contact your treating physician. \nIf life is in immediate danger please call 000")
+  $("input:radio[name='sgE-4977104-5-233']").click(function(){
+    if($("input[id='sgE-4977104-5-233-10223']").is(":checked")) {
+	alert("If you or someone you know needs immediate support, please contact: \nLifeline on 13 11 14, \nSuicide Call Back Service on 1300 659 467, \nor contact your treating physician. \nIf life is in immediate danger please call 000");
       }
   });
 });
