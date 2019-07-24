84e37d6f216afdda7f9e915e1de4579976acbf10
Judilla-Jemifer <35318054+Judilla-Jemifer@users.noreply.github.com>
2019-07-24 04:48:54 -0500

    Create DepressionPopUp.js

---
+++ JavaScript/SurveyGizmo/DepressionPopUp.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+  $(":radio").click(function(){
+    if($("input[id='sgE-4977258-5-229-10223']").is(":checked")) {
+	alert("If you or someone you know needs immediate support, please contact: \nLifeline on 13 11 14, \nSuicide Call Back Service on 1300 659 467, \nor contact your treating physician. \nIf life is in immediate danger please call 000")
+      }
+  });
+});
